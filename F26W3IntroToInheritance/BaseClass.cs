26079a06f830a1ca33295a1dc1fb3dfbda1f3bf9
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-22 10:05:04 -0400

    constructor calls

--- F26W3IntroToInheritance/BaseClass.cs
+++ F26W3IntroToInheritance/BaseClass.cs
@@ -9,10 +9,27 @@ namespace F26W3IntroToInheritance
         private int myPrivateVar;
         protected int myProtectedVar;
         public int myPublicVar;
+
+        // constructor
+        //public BaseClass()
+        //{
+        //    Console.WriteLine("Base class constructor called");
+        //}
+
+        public BaseClass(string msg)
+        {
+            Console.WriteLine("Base class constructor with message: " + msg);
+        }
     }
 
     public class DerivedClass : BaseClass
     {
+        //constructor
+        public DerivedClass(string msg) : base(msg)
+        {
+            Console.WriteLine("Derived class constructor called");
+        }
+
         public void MyMethod()
         {
             //myPrivateVar = 1;
--- F26W3IntroToInheritance/Program.cs
+++ F26W3IntroToInheritance/Program.cs
@@ -4,13 +4,17 @@
     {
         static void Main(string[] args)
         {
-            BaseClass objBase = new BaseClass();
+            //BaseClass objBase = new BaseClass();
             //objBase.myPrivateVar = 1;
             //objBase.myProtectedVar = 2;
-            objBase.myPublicVar = 3;
+            //objBase.myPublicVar = 3;
 
-            DerivedClass objDerived = new DerivedClass();
-            objDerived.myPublicVar = 3;
+            DerivedClass objDerived = new DerivedClass("hello");
+            //objDerived.myPublicVar = 3;
+
+
+
+
         }
     }
 }
