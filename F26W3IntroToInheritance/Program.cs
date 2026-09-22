9011c44e7f9113b8a4e8cbb53d4c88c29d71f381
gursharantatla-sheridan <sing1731@shernet.sheridancollege.ca>
2026-09-22 09:27:56 -0400

    Add project files.

---
+++ F26W3IntroToInheritance/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace F26W3IntroToInheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
